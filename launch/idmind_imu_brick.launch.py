b116e1c491e71e044a8a9d01dca9ba8b5ffd808e
cneves <cneves@idmind,pt>
2026-09-10 13:08:15 +0100

    Changed the launch file for future HardwareSDK

--- launch/idmind_imu_brick.launch.py
+++ launch/idmind_imu_brick.launch.py
@@ -24,18 +24,24 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
 
-    def_imu_cfg = os.path.join(
-        get_package_share_directory('idmind_imu'), 'config', 'idmind_imu.yaml'
-    )
-    imu_cfg = DeclareLaunchArgument(
-        name='imu_cfg', default_value=def_imu_cfg, description='Configuration file for IMU'
-    )
+    # Robot name
+    default_robot_name = ""
+    robot_name = DeclareLaunchArgument(name="robot_name", default_value=default_robot_name, description="Name of this robot, used for namespacing")    
+    ld.add_action(robot_name)    
+
+    # Simulation
+    simulation = DeclareLaunchArgument(name="simulation", default_value="False", description="Is the robot in simulation?")    
+    ld.add_action(simulation)    
+
+    def_imu_cfg = os.path.join(get_package_share_directory('idmind_imu'), 'config', 'idmind_imu.yaml')
+    imu_cfg = DeclareLaunchArgument(name='imu_cfg', default_value=def_imu_cfg, description='Configuration file for IMU')
     ld.add_action(imu_cfg)
 
     imu_node = Node(
         package='idmind_imu',
         executable='imu_brick_node_v2',
         name='idmind_imu',
+        namespace=LaunchConfiguration('robot_name'),
         output='screen',
         emulate_tty=True,
         parameters=[LaunchConfiguration('imu_cfg')],
